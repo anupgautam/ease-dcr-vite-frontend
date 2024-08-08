9773d64859b9f287b17ad9cdc9bc382b1aceb0fc
unknown <binaykhanal18@gmail.com>
2024-08-08 10:48:02 +0545

    resolved conflicts

--- src/pages/Common/Landing/LandingController.jsx
+++ src/pages/Common/Landing/LandingController.jsx
@@ -3,20 +3,18 @@ import Navbar from "../../new-landing/Navbar";
 import FirstPage from "../../new-landing/FirstPage";
 import AboutUs from "../../new-landing/AboutUs";
 import FooterTwo from "../../new-landing/FooterTwo";
-import NewFeature from "../../new-landing/NewFeatures";
+import NewFeatures from "../../new-landing/NewFeatures";
 import NewPricing from "../../new-landing/NewPricing";
-import NewTestimonials from "../../new-landing/NewTestimonials";
 
 const LandingController = () => {
   return (
     <div>
       <Navbar />
       <FirstPage />
-      <NewFeature />
+      <NewFeatures />
       <AboutUs />
       <NewPricing />
       <FooterTwo />
-      {/* <NewTestimonials /> */}
     </div>
   );
 };
