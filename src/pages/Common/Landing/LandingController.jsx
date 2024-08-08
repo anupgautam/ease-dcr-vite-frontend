13a6780a0d1ac642ad0e078bc5ea61622517a443
unknown <binaykhanal18@gmail.com>
2024-08-08 10:46:01 +0545

    resolved landing controller

--- src/pages/Common/Landing/LandingController.jsx
+++ src/pages/Common/Landing/LandingController.jsx
@@ -1,35 +1,24 @@
-<<<<<<< HEAD
-import React from 'react'
-import Navbar from '../../new-landing/Navbar'
-import FirstPage from '../../new-landing/FirstPage'
-import AboutUs from '../../new-landing/AboutUs'
-import FooterTwo from '../../new-landing/FooterTwo'
-
-const LandingController = () => {
-    return (
-        <div>
-           <Navbar/>
-           <FirstPage/>
-           <AboutUs/>
-           <FooterTwo/>
-        </div>
-    )
-}
-=======
 import React from "react";
+import Navbar from "../../new-landing/Navbar";
+import FirstPage from "../../new-landing/FirstPage";
+import AboutUs from "../../new-landing/AboutUs";
+import FooterTwo from "../../new-landing/FooterTwo";
 import NewFeature from "../../new-landing/NewFeatures";
 import NewPricing from "../../new-landing/NewPricing";
 import NewTestimonials from "../../new-landing/NewTestimonials";
 
 const LandingController = () => {
   return (
-    <>
+    <div>
+      <Navbar />
+      <FirstPage />
       <NewFeature />
+      <AboutUs />
       <NewPricing />
+      <FooterTwo />
       {/* <NewTestimonials /> */}
-    </>
+    </div>
   );
 };
->>>>>>> 0f92706 (added components)
 
 export default LandingController;
