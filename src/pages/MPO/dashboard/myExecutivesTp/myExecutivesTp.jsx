8d47398581f37d4b7418b4ee02de70a5cf62b625
lemongautam79 <lemongautam79@gmail.com>
2024-09-09 12:28:11 +0545

    [Edit TP left]

--- src/pages/MPO/dashboard/myExecutivesTp/myExecutivesTp.jsx
+++ src/pages/MPO/dashboard/myExecutivesTp/myExecutivesTp.jsx
@@ -283,7 +283,6 @@ const MyExecutiveTp = () => {
             skip: !selectedOption || !selectedOption?.id || !selectedMonth || !selectedYear || !page || !company_id
         }
     )
-    console.log(TourPlanSearch)
     return (
         <>
             <Grid item xs={10}>
