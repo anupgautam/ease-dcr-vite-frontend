4f16785d2549566b4c69d306557fb0f475214a6c
anupgautam60 <anupgautam60@gmail.com>
2024-12-06 18:38:53 +0545

    base url fixes

--- src/baseURL.js
+++ src/baseURL.js
@@ -1,5 +1,5 @@
-export const BASE_URL = "http://localhost:9000/";
-// export const BASE_URL = "https://api.easesfa.com/";
+// export const BASE_URL = "http://localhost:9000/";
+export const BASE_URL = "https://api.easesfa.com/";
 // export const BASE_URL = "https://d15b-27-34-67-114.ngrok-free.app/";
 // export const BASE_URL = "http://localhost:8000/";
 // export const WEBSOCKET_BASE_URL = "ws://api.easesfa.com/";
