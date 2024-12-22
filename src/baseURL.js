f79db77577f669fb09f943cf4e9495bca698f4b6
lemongautam79 <lemongautam79@gmail.com>
2024-12-22 17:51:26 +0545

    [BaseURL]

--- src/baseURL.js
+++ src/baseURL.js
@@ -1,8 +1,6 @@
 // export const BASE_URL = "http://192.168.1.69:9000/";
-// export const BASE_URL = "http://172.20.10.7:9000/";
 export const BASE_URL = "https://api.easesfa.com/";
 // export const BASE_URL = "https://d15b-27-34-67-114.ngrok-free.app/";
-// export const BASE_URL = "http://localhost:8000/";
-export const WEBSOCKET_BASE_URL = "ws://api.easesfa.com/";
-// export const WEBSOCKET_BASE_URL = "ws://localhost:8888/";
+// export const WEBSOCKET_BASE_URL = "ws://api.easesfa.com/";
+export const WEBSOCKET_BASE_URL = "ws://localhost:8888/";
 // export const WEBSOCKET_BASE_URL = "ws:///d15b-27-34-67-114.ngrok-free.app/";
